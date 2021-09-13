145b6752ccb2ed72bb1879492d13fbdf9a20eac4
Prakash Upadhyay <prakash@PrakashMacBook.local>
2021-09-12 21:59:52 -0500

    Made the app :)

--- src/Components/Table/TForm/TFrom.js
+++ src/Components/Table/TForm/TFrom.js
@@ -1,36 +1,78 @@
-import React from 'react'
+import React from "react";
 
-function TFrom() {
+function TFrom({
+        prodId, setProdId,
+        abbr, setAbbr,
+        effectiveDate, setEffectiveDate,
+        paymentAmount, setPaymentAmount,
+        sourceType, setSourceType,
+        paymentType, setPaymentType,
+        taxYear, setTaxYear,
+        allocMethod, setAllocMethod
+}) {
+    /*
+        
+    */
     return (
         <tbody>
-            <form>
               <tr>
                   <td>
-                      <input type="text" />
+                      <input
+                          type="text"
+                          value={prodId}
+                          onChange={e => setProdId(e.target.value)}
+                      />
                   </td>
                   <td>
-                      <input type="text" />
+                      <input
+                          type="number"
+                          value={abbr}
+                          onChange={e => setAbbr(e.target.value)}
+                      />
                   </td>
                   <td>
-                      <input type="text" />
+                      <input
+                          type="date"
+                          value={effectiveDate}
+                          onChange={e => setEffectiveDate(e.target.value)}
+                      />
                   </td>
                   <td>
-                      <input type="text" />
+                      <input
+                          type="number"
+                          value={paymentAmount}
+                          onChange={e => setPaymentAmount(e.target.value)}
+                      />
                   </td>
                   <td>
-                      <input type="text" />
+                      <input
+                          type="number"
+                          value={sourceType}
+                          onChange={e => setSourceType(e.target.value)}
+                      />
                   </td>
                   <td>
-                      <input type="text" />
+                      <input
+                          type="text"
+                          value={paymentType}
+                          onChange={e => setPaymentType(e.target.value)}
+                      />
                   </td>
                   <td>
-                      <input type="text" />
+                      <input
+                          type="text"
+                          value={taxYear}
+                          onChange={e => setTaxYear(e.target.value)}
+                      />
                   </td>
                   <td>
-                      <input type="text" />
+                      <input
+                          type="text"
+                          value={allocMethod}
+                          onChange={e => setAllocMethod(e.target.value)}
+                      />
                   </td>
                   </tr>
-              </form>          
         </tbody>
     )
 }
--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import THead from "./THead";
 import TBody from "./TBody/TBody";
 import api from "../../Public/api";
@@ -7,6 +7,14 @@ import TFrom from './TForm/TFrom';
 function Table(props) {
 	const [data, setData] = useState([]);
 	const [editing, startEditing] = useState(false)
+	const [prodId, setProdId] = useState();
+	const [abbr, setAbbr] = useState();
+	const [effectiveDate, setEffectiveDate] = useState();
+	const [paymentAmount, setPaymentAmount] = useState();
+	const [sourceType, setSourceType] = useState();
+	const [paymentType, setPaymentType] = useState();
+	const [taxYear, setTaxYear] = useState();
+	const [allocMethod, setAllocMethod] = useState();
 	
 	function load() {
 		api.get("prod/products").then(
@@ -18,18 +26,56 @@ function Table(props) {
 	
 	function setStartEditing() {
 		setData([]);
-		startEditing(!editing)
+		startEditing(true)
+	}
+	
+	function submit() {
+		const final_object = {
+			"productID": prodId,
+			"Abbr": abbr,
+			"PaymentAmount": paymentAmount,
+			"EffectiveDate": effectiveDate,
+			"SourceType": sourceType,
+			"PaymentType": paymentType,
+			"TaxYear": taxYear,
+			"AllocMethod": allocMethod
+		}
+		
+		api.post(
+			"prod/product",
+			final_object
+		).then(
+			res => {
+				console.log(res.data);
+				startEditing(false);
+			}
+		)
 	}
 	
 	return (
 		<div className="table">
 			<table>
 				<THead/>
-				{editing ? <TFrom /> : <TBody rows={data}/>}
+				{
+					editing ?
+						<TFrom
+							prodId={prodId} setProdId={setProdId}
+							abbr={abbr} setAbbr={setAbbr}
+							effectiveDate={effectiveDate} setEffectiveDate={setEffectiveDate}
+							paymentAmount={paymentAmount} setPaymentAmount={setPaymentAmount}
+							sourceType={sourceType} setSourceType={setSourceType}
+							paymentType={paymentType} setPaymentType={setPaymentType}
+							taxYear={taxYear} setTaxYear={setTaxYear}
+							allocMethod={allocMethod} setAllocMethod={setAllocMethod}
+						/> :
+						<TBody rows={data}/>}
 			</table>
 			
 			<div className={"load-button"}>
-				<button onClick={setStartEditing}>{editing ? 'Save' : 'Create'}</button>
+				{editing ?
+					<button onClick={submit}>Submit</button>
+					:
+					<button onClick={setStartEditing}>Create</button>}
 				<button onClick={load}>Load</button>
 			</div>
 		</div>
